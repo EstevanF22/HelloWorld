b9cf081c80269968ede477ea262aa56495080296
EstevanF22 <132707064+EstevanF22@users.noreply.github.com>
2023-05-05 14:03:05 -0600

    Update jenkinsfile.groovy

--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -1,10 +1,11 @@
 pipeline {
     agent any
-
-    stage('Deploy to Kubernetes') {
-    steps {
-        sh "kubectl apply -f deployment.yaml"
-        sh "kubectl apply -f service.yaml"
+    
+    stages {
+        stage('Deploy to Kubernetes') {
+           steps {
+           sh "kubectl apply -f deployment.yaml"
+           sh "kubectl apply -f service.yaml"
     }
 }
 }
