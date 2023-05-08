58c7cdfd83aac49d42793073be517d5cbf816549
EstevanF22 <132707064+EstevanF22@users.noreply.github.com>
2023-05-08 13:49:36 -0600

    Update jenkinsfile.groovy

--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -38,66 +38,6 @@ pipeline {
     }
     post {
         always {
-            script {
-                def crb = """
-                apiVersion: rbac.authorization.k8s.io/v1
-                kind: ClusterRoleBinding
-                metadata:
-                  name: jenkins-admin-binding
-                roleRef:
-                  apiGroup: rbac.authorization.k8s.io
-                  kind: ClusterRole
-                  name: cluster-admin
-                subjects:
-                - kind: ServiceAccount
-                  name: jenkins-admin
-                  namespace: default
-                """
-                sh "./kubectl apply -f - <<EOF\n${crb}\nEOF"
-            }
-        }
-    }
-}
-pipeline {
-    agent {
-        kubernetes {
-            label 'minikube'
-            defaultContainer 'jnlp'
-            yaml """
-            apiVersion: v1
-            kind: Pod
-            metadata:
-              labels:
-                app: jenkins
-            spec:
-              serviceAccountName: jenkins-admin
-              containers:
-              - name: kubectl
-                image: lachlanevenson/k8s-kubectl
-                command:
-                - cat
-                tty: true
-            """
-        }
-    }
-    stages {
-        stage('Build') {
-            steps {
-                sh 'echo "Nothing to build"'
-            }
-        }
-        stage('Deploy') {
-            steps {
-                sh 'curl -LO "https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl"'
-                sh 'chmod +x kubectl'
-                sh 'kubectl apply -f jenkins-admin-rolebinding.yml'
-                sh './kubectl apply -f hello-world-deployment.yml'
-                sh './kubectl apply -f hello-world-service.yml'
-            }
-        }
-    }
-    post {
-        always {
             node {
                 def crb = """
                 apiVersion: rbac.authorization.k8s.io/v1
