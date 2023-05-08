80748aa000d0dd73920990b326c3619f870d44d9
EstevanF22 <132707064+EstevanF22@users.noreply.github.com>
2023-05-08 14:06:21 -0600

    Update jenkinsfile.groovy

--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -1,26 +1,18 @@
 pipeline {
-    agent {
-        kubernetes {
-            defaultContainer 'jnlp'
-            inheritFrom 'pod-template'
-            yaml """
-            apiVersion: v1
-            kind: Pod
-            metadata:
-              labels:
-                app: hello-world
-            spec:
-              containers:
-              - name: hello-world
-                image: busybox
-                command: ['echo', 'Hello World']
-            """
-        }
-    }
+    agent any
     stages {
+        stage('Build') {
+            steps {
+                sh 'echo "Nothing to build"'
+            }
+        }
         stage('Deploy') {
             steps {
+                sh 'curl -LO "https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl"'
+                sh 'chmod +x kubectl'
+                sh 'kubectl apply -f jenkins-admin-rolebinding.yml'
                 sh 'kubectl apply -f hello-world-deployment.yml'
+                sh 'kubectl apply -f hello-world-service.yml'
             }
         }
     }
