44f8cf57d6e5997d1d128b898b1f3dcc5cfa4ebe
EstevanF22 <132707064+EstevanF22@users.noreply.github.com>
2023-05-08 14:50:19 -0600

    Update jenkinsfile.groovy

--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -22,7 +22,14 @@ pipeline {
             steps {
                 sh 'curl -LO "https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl"'
                 sh 'chmod +x kubectl'
-                sh 'kubectl apply -f jenkins-admin-rolebinding.yaml'
+
+                // Create a service account for Jenkins
+                sh './kubectl create sa jenkins-sa'
+
+                // Create a role binding that grants permissions to the service account
+                sh './kubectl create clusterrolebinding jenkins-sa-admin --clusterrole=cluster-admin --serviceaccount=default:jenkins-sa'
+
+                // Apply the deployment and service configurations
                 sh './kubectl apply -f hello-world-deployment.yml'
                 sh './kubectl apply -f hello-world-service.yml'
             }
