a01a4c9411738c9556f956c8379b4af45ea0efad
EstevanF22 <132707064+EstevanF22@users.noreply.github.com>
2023-05-08 14:44:00 -0600

    Update jenkinsfile.groovy

--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -1,60 +1,30 @@
 pipeline {
     agent {
         kubernetes {
-            label 'minikube'
             defaultContainer 'jnlp'
+            inheritFrom 'pod-template'
             yaml """
             apiVersion: v1
             kind: Pod
             metadata:
               labels:
-                app: jenkins
+                app: hello-world
             spec:
-              serviceAccountName: jenkins-admin
               containers:
-              - name: kubectl
-                image: lachlanevenson/k8s-kubectl
-                command:
-                - cat
-                tty: true
+              - name: hello-world
+                image: busybox
+                command: ['echo', 'Hello World']
             """
         }
     }
     stages {
-        stage('Build') {
-            steps {
-                sh 'echo "Nothing to build"'
-            }
-        }
         stage('Deploy') {
             steps {
                 sh 'curl -LO "https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl"'
                 sh 'chmod +x kubectl'
-                sh 'kubectl apply -f jenkins-admin-rolebinding.yml'
+                sh 'kubectl apply -f jenkins-admin-rolebinding.yaml'
                 sh './kubectl apply -f hello-world-deployment.yml'
                 sh './kubectl apply -f hello-world-service.yml'
-            }
-        }
-        stage('Configure Permissions') {
-            steps {
-                script {
-                    def crb = """
-                    apiVersion: rbac.authorization.k8s.io/v1
-                    kind: ClusterRoleBinding
-                    metadata:
-                      name: jenkins-admin-binding
-                    roleRef:
-                      apiGroup: rbac.authorization.k8s.io
-                      kind: ClusterRole
-                      name: cluster-admin
-                    subjects:
-                    - kind: ServiceAccount
-                      name: jenkins-admin
-                      namespace: default
-                    """
-                    writeFile file: 'crb.yaml', text: crb
-                    sh './kubectl apply -f crb.yaml'
-                }
             }
         }
     }
