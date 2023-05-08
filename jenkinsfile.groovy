faa1bd8d8b33b76b95437145512f82b34b2c6548
EstevanF22 <132707064+EstevanF22@users.noreply.github.com>
2023-05-08 13:25:00 -0600

    Update jenkinsfile.groovy

--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -11,8 +11,8 @@ pipeline {
                 sh 'curl -LO "https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl"'
                 sh 'chmod +x kubectl'
                 sh 'ls -la'
-                sh './kubectl apply -f hello-world-deployment.yaml'
-                sh './kubectl apply -f hello-world-service.yaml'
+                sh './kubectl apply -f hello-world-deployment.yml'
+                sh './kubectl apply -f hello-world-service.yml'
             }
         }
     }
