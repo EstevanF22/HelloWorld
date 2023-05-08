75fbf99d7a62ae9181485b52a541dc99e64d1732
EstevanF22 <132707064+EstevanF22@users.noreply.github.com>
2023-05-08 13:35:33 -0600

    Update jenkinsfile.groovy

--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -12,6 +12,7 @@ pipeline {
                     def kubectl_version = 'v1.22.5' // replace with desired version
                     sh "curl -LO https://storage.googleapis.com/kubernetes-release/release/${kubectl_version}/bin/linux/amd64/kubectl"
                     sh 'chmod +x kubectl'
+                    sh 'kubectl apply -f jenkins-admin-rolebinding.yml'
                     sh './kubectl apply -f hello-world-deployment.yml'
                     sh './kubectl apply -f hello-world-service.yml'
                 }
