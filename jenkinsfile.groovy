029697a68ecd6087577a726dabc4f79c6157aa4d
EstevanF22 <132707064+EstevanF22@users.noreply.github.com>
2023-05-10 10:09:28 -0600

    Update jenkinsfile.groovy

--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -14,12 +14,5 @@ pipeline {
                 sh './kubectl apply -f hello-world-service.yml'
             }
         }
-        stage('Configure Permissions') {
-            steps {
-                sh 'curl -LO "https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl"'
-                sh 'chmod +x kubectl'
-                sh "./kubectl create clusterrolebinding jenkins-admin-binding --clusterrole cluster-admin --serviceaccount=default:jenkins-admin"
-            }
-        }
     }
 }
