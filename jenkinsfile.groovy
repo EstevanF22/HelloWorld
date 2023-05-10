5c36625eb4d33a7414ccd1f0a3423e01af074e6d
EstevanF22 <132707064+EstevanF22@users.noreply.github.com>
2023-05-10 10:24:03 -0600

    Update jenkinsfile.groovy

--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -10,7 +10,7 @@ pipeline {
             steps {
                 sh 'curl -LO "https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl"'
                 sh 'chmod +x kubectl'
-                sh './kubectl create deployment hello-node --image=registry.k8s.io/e2e-test-images/agnhost:2.39 -- /agnhost netexec --http-port=8080
+                sh './kubectl create deployment hello-node --image=registry.k8s.io/e2e-test-images/agnhost:2.39 -- /agnhost netexec --http-port=8080'
             }
         }
     }
